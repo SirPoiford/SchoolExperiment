2ce9ffbb6b9245f6c0b0860bbe50fea4db5f1930
SirPoiford <kidanesamira@gmail.com>
2022-01-18 15:33:00 +0000

    create main

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("Hello World. Goodbye World.")
-print("This is a pen")
+from bs4 import BeautifulSoup 
+from requests import get 
+url="https://xkcd.com/archive/"
+res=get(url)
+soup=BeautifulSoup(res.content, "html.parser")
+hrefs=[elem['href'] 
+for elem in soup.select(r"a[title]")] 
+print(hrefs)
